fixed R0 calculation

src/CalculateR_0s.py
@@ -4,7 +4,7 @@ This will calculate the R_0 values empirically for the given diseases
 
 from Simulation import *
 
-diseases_to_measure = [virus_0,virus_1,competitive_disease_0,STD_0]
+diseases_to_measure = [virus_1]
 max_days = 30
 with_vaccination = False												#should we allow these diseases to have any of the population vaccinated in our measurement
 SIM_CONFIG = 'small'
@@ -16,36 +16,32 @@ if not with_vaccination:
 		disease.vaccination_rate = 0.#no one's vaccinated in this case
 
 for disease in diseases_to_measure:
-	for dii in [False,True]:
-		DISABLE_IDLE_INFECTION = dii#do both idle and direct R_0
-		dis = [disease]
-		s = Simulation()
+	dis = [disease]
+	s = Simulation()
 
-		if SIM_CONFIG == 'full':
-			#setup map reader
-			mr = MapReader()
-			map_fname = 'test_map_0.png'
-			s.read_map_from_image(mr,map_fname)
+	if SIM_CONFIG == 'full':
+		#setup map reader
+		mr = MapReader()
+		map_fname = 'test_map_0.png'
+		s.read_map_from_image(mr,map_fname)
 
-		elif SIM_CONFIG == 'small':
-			# setup map reader
-			mr = MapReader(PUBLIC_BLOCK_SIZE=(2, 2), CAPACITY_PER_PIXEL=2, TIME_STEP_PER_PIXEL=2)
-			map_fname = 'test_map_small.png'
-			s.read_map_from_image(mr, map_fname)
+	elif SIM_CONFIG == 'small':
+		# setup map reader
+		mr = MapReader(PUBLIC_BLOCK_SIZE=(2, 2), CAPACITY_PER_PIXEL=2, TIME_STEP_PER_PIXEL=2)
+		map_fname = 'test_map_small.png'
+		s.read_map_from_image(mr, map_fname)
 
-		# setup diseases
-		s.set_diseases(dis)
+	# setup diseases
+	s.set_diseases(dis)
 
-		# setup pop builder
-		pb = PopulationBuilder(s.map, pop_size)
-		pb.set_num_friends_distribution(lambda possible_friends: min(max(int(np.random.normal(6, 3)), 0), possible_friends))
-		pb.set_num_coworkers_distribution(lambda possible: min(max(int(np.random.normal(6, 5)), 0), possible))
-		s.create_population(pb)
+	# setup pop builder
+	pb = PopulationBuilder(s.map, pop_size)
+	pb.set_num_friends_distribution(lambda possible_friends: min(max(int(np.random.normal(6, 3)), 0), possible_friends))
+	pb.set_num_coworkers_distribution(lambda possible: min(max(int(np.random.normal(6, 5)), 0), possible))
+	pb.set_partners_distribution(lambda num_people_in_house: min(int(coinflip(0.15)),num_people_in_house))
+	s.create_population(pb)
 
-		#YEET
-		s.full_simulation(verbose=True,time_limit=max_days)
+	#YEET
+	r0 = s.full_simulation(verbose=True,time_limit=max_days,store_R_0s=True)[disease]
 
-		if dii:
-			print('Measured direct R_0 for ' + disease.name + ': ' + str(disease.R_0_measure))
-		else:
-			print('Measured idle R_0 for ' + disease.name + ': ' + str(disease.R_0_measure))
+	print('Measured R_0 for ' + disease.name + ': ' + str(r0))

src/Simulation.py
@@ -226,11 +226,12 @@ class Simulation:
 		else:
 			return False
 
-	def full_simulation(self,converged=converged_strict_single_dead,verbose = False,time_limit=0):
+	def full_simulation(self,converged=converged_strict_single_dead,verbose = False,time_limit=0,store_R_0s=False):
 		#pick a patient zero for each disease
 		diseases_running = 0	#how many diseases actually have anyone infected?
 		diseases_not_running = []#which diseases of the ones requested aren't actually running due to immunity?
 		for d in self.diseases:
+			d.calculate_R_0 = store_R_0s
 			choices = []
 			if self.ensure_non_immune_patient_zero:
 				for person in self.population:
@@ -256,8 +257,30 @@ class Simulation:
 		dayct = 0
 		if verbose:
 			print()
+		R_0s = {disease:[] for disease in self.diseases}		#map from disease to list of (total number of people infected currently,R0 value) at the end of each day
+		true_R_0 = {disease:0. for disease in self.diseases}
+		max_infected = {disease:0 for disease in self.diseases}
 		while (not converged(self)) and ((time_limit <= 0) or (dayct < time_limit)):
 			self.simulate_day()
+			if store_R_0s:
+				for disease in self.diseases:
+					# calculate the R_0 value for today
+					if len(disease.num_infected_by) > 0:
+						today_r0 = np.mean(list(disease.num_infected_by.values()))
+						infected_now = 0
+						all_infected = True
+						for person in self.population:
+							if person.disease_state[disease] in DISEASE_STATES_INFECTIOUS:
+								infected_now += 1
+							if person.disease_state[disease] in DISEASE_STATES_SUSCEPTIBLE:
+								all_infected = False
+						if infected_now > max_infected[disease]:
+							true_R_0[disease] = today_r0
+							max_infected[disease] = infected_now
+						R_0s[disease].append((infected_now,today_r0))
+						if all_infected:
+							return true_R_0	#this parameter is only set if we're trying to calculate r0 anyway, and we have our estimate so we're done
+
 			if verbose:
 				print('day ' + str(dayct) + ' summary: ')
 				print('total infections today:\t\t' + str(self.total_idle_infections_today + self.total_direct_infections_today))
@@ -277,3 +300,6 @@ class Simulation:
 				print('current infections: ' + str(total_infected))	#will be off by however many diseases are actually going
 				print()
 			dayct += 1
+
+		if store_R_0s:
+			return true_R_0